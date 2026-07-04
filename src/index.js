bbd5a484dcafb66302afaa9eb551d0ebf80e62bc
kimbomm <qhals0710@gmail.com>
2026-07-04 12:53:40 +0900

    #1.2 redux actions

--- src/index.js
+++ src/index.js
@@ -4,11 +4,26 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  console.log("count", count);
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
-console.log("countStore", countStore);
-console.log("getState", countStore.getState());
+
+// dispatch를 통해 action을 보내면 countModifier가 실행된다.
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState());
