068addac0cd928c4983af4888b5d75527dcd8ce3
kimbomm <qhals0710@gmail.com>
2026-07-06 11:50:37 +0900

    #2.3 redux delete todo

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const DEL_TODO = "DEL_TODO";
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }];
+      return [{ text: action.text, id: Date.now() }, ...state];
     case DEL_TODO:
-      return [];
+      return state.filter((todo) => todo.id !== action.id);
     default:
       return state;
   }
@@ -21,16 +21,37 @@ const reducer = (state = [], action) => {
 const store = createStore(reducer);
 // dispatch, getState, subscribe, replaceReducer
 
-store.subscribe(() => {
+const paintToDos = () => {
   const toDos = store.getState();
-  console.log(toDos);
-});
+  ul.innerHTML = "";
+  toDos.forEach((todo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", deleteTodo);
+    li.id = todo.id;
+    li.innerText = todo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+
+store.subscribe(paintToDos);
+
+const addTodo = (text) => {
+  store.dispatch({ type: ADD_TODO, text });
+};
+
+const deleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch({ type: DEL_TODO, id });
+};
 
 const handleSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  addTodo(toDo);
 };
 
 form.addEventListener("submit", handleSubmit);
