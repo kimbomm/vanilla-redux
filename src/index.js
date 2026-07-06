bc7c84d42876e792cbcd5fa36378b0bc4a2d1eee
kimbomm <qhals0710@gmail.com>
2026-07-06 11:05:53 +0900

    #1.3 redux subscribes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
   if (action.type === "ADD") {
     return count + 1;
   } else if (action.type === "MINUS") {
@@ -16,14 +15,23 @@ const countModifier = (count = 0, action) => {
 };
 
 const countStore = createStore(countModifier);
+// dispatch, getState, subscribe, replaceReducer
 
-// dispatch를 통해 action을 보내면 countModifier가 실행된다.
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "ADD" });
-countStore.dispatch({ type: "MINUS" });
+const onChange = () => {
+  const count = countStore.getState();
 
-console.log(countStore.getState());
+  number.innerText = count;
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
